9e7abdc9b26588934bb78a56fd2beab6cdfa0b9b
Christopher Krawietz <krawietz@new-communication.de>
2017-07-26 15:47:43 +0200

    finished Lesson5 and finished contacts app

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,36 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, {hash: true})
+    if(this.props.OnCreateContact)
+      this.props.OnCreateContact(values)
+  }
   render() {
     return (
-      <div>Create Contact!</div>
+      <div>
+        <Link
+          className="close-create-contact"
+          to="/">Zurück
+        </Link>
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
+          <ImageInput
+            className="create-contact-avatar-input"
+            name="avatarURL"
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Vor- und Nachname"/>
+            <input type="text" name="email" placeholder="Email-Adresse"/>
+            <button>Kontakt speichern</button>
+          </div>
+
+        </form>
+      </div>
     )
   }
 }
